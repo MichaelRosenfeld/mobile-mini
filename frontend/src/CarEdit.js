03f81d01bab409afc7a0076038b62cee7e2b6326
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2022-01-26 08:42:27 +0100

    Add CarEdit Component

---
+++ frontend/src/CarEdit.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+
+class CarEdit extends Component {
+
+    emptyItem = {
+        make: '',
+        model: '',
+        constructionYear: '',
+        description: '',
+        price: '',
+        email: ''
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: this.emptyItem
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async componentDidMount() {
+        if (this.props.match.params.id !== 'new') {
+            const car = await (await fetch(`/cars/${this.props.match.params.id}`)).json();
+            this.setState({item: car});
+        }
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        let item = {...this.state.item};
+        item[name] = value;
+        this.setState({item});
+    }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        const {item} = this.state;
+
+        await fetch('/cars' + (item.id ? '/' + item.id : ''), {
+            method: (item.id) ? 'PUT' : 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item),
+        });
+        this.props.history.push('/cars');
+    }
+
+    render() {
+        const {item} = this.state;
+        const title = <h2>{item.id ? 'Edit Car' : 'Add Car'}</h2>;
+
+        return <div>
+            <AppNavbar/>
+            <Container>
+                {title}
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="make">Make</Label>
+                        <Input type="text" name="make" id="make" value={item.make || ''}
+                               onChange={this.handleChange} autoComplete="make"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="model">Model</Label>
+                        <Input type="text" name="model" id="model" value={item.model|| ''}
+                               onChange={this.handleChange} autoComplete="model"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="construction-year">Construction Year</Label>
+                        <Input type="text" name="construction-year" id="construction-year" value={item.constructionYear|| ''}
+                               onChange={this.handleChange} autoComplete="construction-year"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="description">Description</Label>
+                        <Input type="text" name="description" id="description" value={item.description|| ''}
+                               onChange={this.handleChange} autoComplete="description"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="price">Price</Label>
+                        <Input type="text" name="price" id="price" value={item.price|| ''}
+                               onChange={this.handleChange} autoComplete="price"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email</Label>
+                        <Input type="text" name="email" id="email" value={item.email|| ''}
+                               onChange={this.handleChange} autoComplete="email"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>{' '}
+                        <Button color="secondary" tag={Link} to="/clients">Cancel</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
+    }
+
+
+}
+export default withRouter(CarEdit);
